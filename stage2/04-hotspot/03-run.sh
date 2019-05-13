783f9051bfbf76ca1330bf3ee03460419b19cee9
yeppao <syln3o@gmail.com>
2019-05-13 18:50:42 +0200

    Change some configuration on wifi tools

--- stage2/04-hotspot/03-run.sh
+++ stage2/04-hotspot/03-run.sh
@@ -5,10 +5,6 @@ install -v -m 755 files/dhcpcd.conf	"${ROOTFS_DIR}/etc/"
 # mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig  
 install -v -m 755 files/dnsmasq.conf "${ROOTFS_DIR}/etc/"
 
-# HostAPD
-install -v -m 755 files/hostapd/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
-install -v -m 755 files/default/hostapd "${ROOTFS_DIR}/etc/default/"
-
 # IPTables
 install -v -m 755 files/iptables-save "${ROOTFS_DIR}/etc/"
 
@@ -24,12 +20,8 @@ echo "echo 'RUNNING iptables restore now'" >> /etc/network/if-up.d/iptables
 echo "iptables-restore < /etc/iptables-save" >> /etc/network/if-up.d/iptables
 echo "exit 0;" >> /etc/network/if-up.d/iptables
 
-systemctl enable dhcpcd
-systemctl enable hostapd
-
 curl -sSL https://get.docker.com | sh
 usermod -aG docker nadia
-
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -  # Install NodeJS v10
 sudo apt-get install -y nodejs
 npm install npm@latest -g
